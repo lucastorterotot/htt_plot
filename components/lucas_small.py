b89eefe3f79c751f5b9c5de49498e7b6560db146
lucastorterotot <lucas.torterotot@gmail.com>
2018-07-16 18:37:24 +0200

    corrected lucas_small components

--- components/lucas_small.py
+++ components/lucas_small.py
@@ -9,17 +9,17 @@ Nevts_factor = 1./100
 DYJetsToLL_M10to50_LO = Component(
     'DYJetsToLL_M10to50_LO',
     basedir+'DYJetsToLL_M10to50_LO.root',
-    35291566/Nevts_factor, 16270.0
+    35291566*Nevts_factor, 18610.
 )
 DYJetsToLL_M50_LO_ext = Component(
     'DYJetsToLL_M50_LO_ext',
     basedir+'DYJetsToLL_M50_LO_ext.root',
-    94531994/Nevts_factor, 4963.0
+    94531994*Nevts_factor, 5765.4
 )
 DYJetsToLL_M50_LO_ext2 = Component(
     'DYJetsToLL_M50_LO_ext2',
     basedir+'DYJetsToLL_M50_LO_ext2.root', 
-    65473457/Nevts_factor, 4963.0 # 5765.4
+    65473457*Nevts_factor, 5765.4
 )
 # Colin put 49144274 evts ?
 
@@ -29,32 +29,36 @@ DY.name = 'DY'
 TT_pow = Component(
     'TT_pow',
     basedir+'TT_pow.root',
-    74644514/Nevts_factor, 730.6
+    74644514*Nevts_factor, 831.76
 )
 WJetsToLNu_LO = Component(
     'WJetsToLNu_LO',
     basedir+'WJetsToLNu_LO.root',
-    28427574/Nevts_factor, 50260.0
+    28427574*Nevts_factor, 61526.7
 )
 WJetsToLNu_LO_ext = Component(
     'WJetsToLNu_LO_ext',
     basedir+'WJetsToLNu_LO_ext.root',
-    57026058/Nevts_factor, 50260.0
+    57026058*Nevts_factor, 61526.7
 )
 data1 = Component(
     'data1',
-    basedir+'data_single_muon_1.root'
+    basedir+'data_single_muon_1.root',
+    norm_factor = 1/Nevts_factor
 )
 data2 = Component(
     'data1',
-    basedir+'data_single_muon_2.root'
+    basedir+'data_single_muon_2.root',
+    norm_factor = 1/Nevts_factor
 )
 data3 = Component(
     'data1',
-    basedir+'data_single_muon_3.root'
+    basedir+'data_single_muon_3.root',
+    norm_factor = 1/Nevts_factor
 )
 data4 = Component(
     'data1',
-    basedir+'data_single_muon_4.root'
+    basedir+'data_single_muon_4.root',
+    norm_factor = 1/Nevts_factor
 )
 
