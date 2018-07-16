ced35cd17617fb89d270e7b8d793122499f0125e
lucastorterotot <lucas.torterotot@gmail.com>
2018-07-16 18:36:26 +0200

    updated component class to get weight

--- components/component.py
+++ components/component.py
@@ -2,15 +2,26 @@ from ROOT import TFile
 
 class Component(object):
     
-    def __init__(self, name, rootfname, nevts=None, xsection=None):
+    def __init__(self, name, rootfname, nevts=None, xsection=None, norm_factor = 1.):
         self.name = name
         self.rootfname = rootfname
         self.nevts = nevts
         self.xsection = xsection
+        self.norm_factor = norm_factor
+        if xsection and nevts:
+            self.is_data = False
+        elif xsection or nevts:
+            raise ValueError('provide both xsection and nevts, or neither')
+        else:
+            self.is_data = True
         self.tfile = TFile(rootfname)
         self.tree = self.tfile.Get('tree')
-        if nevts is not None and xsection is not None:
-            self.lumi_eq = nevts/xsection
         
-    def compute_weight(self, lumi_data):
-        self.weight = self.xsection*lumi_data
+    def compute_weight(self, lumi_data = None):
+        if self.is_data:
+            self.weight = self.norm_factor
+        else:
+            if not lumi_data:
+                raise ValueError('provide lumi to weight MC component')
+            self.weight = self.xsection*lumi_data/self.nevts*self.norm_factor
+        return self.weight
