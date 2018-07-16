c2561c27a6c18cda624f60f87e20df0f45b3af84
Colin Bernet <colin.bernet@cern.ch>
2018-07-16 12:50:29 +0200

    new cut class

--- tools/cut.py
+++ tools/cut.py
@@ -1,19 +1,23 @@
+from collections import OrderedDict
 import copy
-
+import pprint 
 
 class Cut(object):
-
-    def __init__(self, cutstr):
+    
+    def __init__(self, name, cutstr):
+        self.name = name
         self.cutstr = cutstr
 
     def __and__(self, other):
         newone = copy.deepcopy(self)
-        newone.cutstr = '({cut1}) && ({cut2})'.format(cut1=str(self), cut2=str(other))
+        newone.cutstr = '({cut1}) && ({cut2})'.format(cut1=str(self),
+                                                      cut2=str(other))
         return newone
 
     def __or__(self, other):
         newone = copy.deepcopy(self)
-        newone.cutstr = '(({cut1}) || ({cut2}))'.format(cut1=str(self), cut2=str(other))
+        newone.cutstr = '(({cut1}) || ({cut2}))'.format(cut1=str(self),
+                                                        cut2=str(other))
         return newone
 
     def __str__(self):
@@ -24,9 +28,35 @@ class Cut(object):
         newone.cutstr = '!({cut})'.format(cut=str(self))
         return newone
     
-    # RIC: this is a bit dangerous as it depends exactly on
-    #      how the string is typed in. 
-    def replace(self, oldcut, newcut):
-        newone = copy.deepcopy(self)
-        newone.cutstr = newone.cutstr.replace(oldcut.cutstr, newcut.cutstr)
-        return newone
+
+class CutFlow(OrderedDict):
+    '''Represents a cut flow.
+    The cuts are Cut objects, and are ordered.
+    a new cut can be added by doing:
+    
+    cut['newcut'] = 'abs(eta)<0.5'
+    
+    a cut can be deleted by doing:
+    
+    del cut['newcut']
+    '''
+    def __init__(self, items):
+        '''e.g.
+        cuts = Cuts([
+            ('pcut', 'p>1'),
+            ('ecut', 'e<0')
+        ])
+        '''
+        tmp = [ (name, Cut(name, cutstr)) for name, cutstr in items]
+        super(CutFlow, self).__init__(items)
+        
+    def marginal(self, cutname):
+        marg_cuts = copy.copy(self)
+        del marg_cuts[cutname]
+        return marg_cuts
+
+    def __str__(self):
+        '''not sure root will accept a multiline string..'''
+        tmp = ['({})'.format(cut) for cut in self.values()]
+        return ' && \ \n'.join(tmp)
+   
---
+++ tools/test_cut.py
@@ -0,0 +1,26 @@
+import unittest
+import pprint 
+from cut import *
+
+class TestCut(unittest.TestCase):
+    
+    def test_1_cut(self):
+        cut = Cut('a', 'p>1')
+        self.assertEqual(str(cut), 'p>1')
+        neg = ~cut
+        self.assertEqual(str(neg), '!(p>1)')
+        cut2 = Cut('b', 'e<0')
+        cut_and_cut2 = cut & cut2
+        self.assertEqual(str(cut_and_cut2), '(p>1) && (e<0)')
+        neg = ~cut_and_cut2
+        self.assertEqual(str(neg), '!((p>1) && (e<0))')
+        
+    def test_2_cutflow(self):
+        cuts = CutFlow([
+            ('pcut', 'p>1'),
+            ('ecut', 'e<0')
+        ])
+        
+    
+if __name__ == '__main__':
+    unittest.main()
