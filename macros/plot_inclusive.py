a2d008444d2ecaa2a66237e14272d854a80bd87d
lucastorterotot <lucas.torterotot@gmail.com>
2018-07-17 17:20:03 +0200

    added WJ renormalization and QCD estimation

--- macros/plot_inclusive.py
+++ macros/plot_inclusive.py
@@ -2,7 +2,8 @@
 import htt_plot.tools.config as config
 config.parallel = False
 
-from htt_plot.components.lucas_all import *
+from htt_plot.datasets.lucas_all import *
+
 from htt_plot.tools.cut import Cut
 from htt_plot.tools.plot import hist, add
 
@@ -10,28 +11,32 @@ from htt_plot.cuts.generic import cuts_generic, cut_os, cut_ss
 from htt_plot.cuts.mt import cuts_mt
 from htt_plot.cuts.mt_triggers import triggers
 
+from htt_plot.tools.plotting.plotter import Plotter
+from htt_plot.tools.plotting.tdrstyle import setTDRStyle
+setTDRStyle(square=True)
+
 import copy
 
-cuts = cuts_generic + cuts_mt + triggers
+cuts = cuts_generic + cuts_mt #+ triggers
 var = 'mt_total'
 cuts_os = copy.copy(cuts)
 cuts_os['os'] = cut_os
 cut = str(cuts_os)
 
-weight='weight'
+weight='weight*weight_dy'
 
 # adding weight
-cut = '({cut})*({weight}*weight_dy)'.format(cut=cut,weight=weight)
+cut = '({cut})*({weight})'.format(cut=cut,weight=weight)
 
 bins = 50, 0., 500.
 
 lumi = 35900.
 
 ##############
-# 
+# Declaring datasets to weight
 ##############
 
-mc_components = [
+mc_datasets = [
     DYJetsToLL_M10to50_LO,
     DYJetsToLL_M50_LO_ext,
     DYJetsToLL_M50_LO_ext2,
@@ -40,13 +45,13 @@ mc_components = [
     WJetsToLNu_LO_ext,
 ]
 
-for component in mc_components:
-    component.compute_weight(lumi)
+for dataset in mc_datasets:
+    dataset.compute_weight(lumi)
 
-data_components = [data1, data2, data3, data4]
+data_datasets = [data1, data2, data3, data4]
 
-for component in data_components:
-    component.compute_weight()
+for dataset in data_datasets:
+    dataset.compute_weight()
 
 ##############
 # Data histo
@@ -74,10 +79,10 @@ print h_data.Integral()
 # MC histos
 ##############
 
-def sumMCweights(components, lumi_data = lumi):
+def sumMCweights(datasets, lumi_data = lumi):
     lumi_eq_tot = 0.
-    for component in components:
-        lumi_eq_tot += component.nevts/(component.xsection*component.norm_factor)
+    for dataset in datasets:
+        lumi_eq_tot += dataset.nevts/(dataset.xsection*dataset.norm_factor)
     return lumi_data/lumi_eq_tot
 
 ## DY
@@ -95,6 +100,8 @@ h_DY_mht50 = hist('DY_mht50', DYJetsToLL_M50_LO_ext, var, cut, *bins)
 h_DY_mht50.Scale(DYJetsToLL_M50_LO_ext.weight)
 
 h_DY = add('DY', [h_DY_mlt50, h_DY_mht50])
+if config.parallel:
+    h_DY = h_DY.compute()
 print h_DY.GetEntries()
 print h_DY.Integral()
 #h_DY.Draw()
@@ -103,22 +110,28 @@ print h_DY.Integral()
 
 h_TT = hist('TT', TT_pow, var, cut, *bins)
 h_TT.Scale(TT_pow.weight)
-
+if config.parallel:
+    h_TT = h_TT.compute()
 print h_TT.GetEntries()
 print h_TT.Integral()
 #TT_pow.Draw()
 
 ## WJ
 
-h_WJ1 = hist('WJ1', WJetsToLNu_LO, var, cut, *bins)
-h_WJ2 = hist('WJ2', WJetsToLNu_LO_ext, var, cut, *bins)
+# h_WJ1 = hist('WJ1', WJetsToLNu_LO, var, cut, *bins)
+# h_WJ2 = hist('WJ2', WJetsToLNu_LO_ext, var, cut, *bins)
 
-h_WJ = add('WJ', [h_WJ1, h_WJ2])
-h_WJ.Scale(sumMCweights([WJetsToLNu_LO,WJetsToLNu_LO_ext]))
-
+# h_WJ = add('WJ', [h_WJ1, h_WJ2])
+# h_WJ.Scale(sumMCweights([WJetsToLNu_LO,WJetsToLNu_LO_ext]))
+h_WJ = hist('WJ', WJetsToLNu_LO_ext, var, cut, *bins)
+h_WJ.Scale(WJetsToLNu_LO_ext.weight)
+if config.parallel:
+    h_WJ = h_WJ.compute()
 print h_WJ.GetEntries()
 print h_WJ.Integral()
 #h_WJ.Draw()
+
+##
 
 h_bg = add('bg',[h_DY,h_TT,h_WJ])
 h_DYTT = add('dytt',[h_DY,h_TT])
@@ -127,11 +140,190 @@ h_DYTT = add('dytt',[h_DY,h_TT])
 # WJ renormalization
 ##############
 
+auto_WJ_SF = True
 
+var_WJ_SF = 'mt'
+
+cuts_WJ_SF = copy.copy(cuts)
+cuts_WJ_SF['low_mt'] = 'mt>70'
+cuts_WJ_SF['os'] = cut_os
+
+cut_WJ_SF = str(cuts_WJ_SF)
+cut_WJ_SF = '({cut})*({weight})'.format(cut=cut_WJ_SF,weight=weight)
+
+if auto_WJ_SF:
+    h_data1_WJ_SF = hist('data1_WJ_SF', data1, var_WJ_SF, cut_WJ_SF, *bins)
+    h_data2_WJ_SF = hist('data2_WJ_SF', data2, var_WJ_SF, cut_WJ_SF, *bins)
+    h_data3_WJ_SF = hist('data3_WJ_SF', data3, var_WJ_SF, cut_WJ_SF, *bins)
+    h_data4_WJ_SF = hist('data4_WJ_SF', data4, var_WJ_SF, cut_WJ_SF, *bins)
+
+    all_data_WJ_SF = [h_data1_WJ_SF, h_data2_WJ_SF, h_data3_WJ_SF, h_data4_WJ_SF]
+
+    h_data_WJ_SF = add('data_WJ_SF', all_data_WJ_SF)
+    h_data_WJ_SF.Scale(1)
+    
+    h_DY_mlt50_WJ_SF   = hist('DY_mlt50_WJ_SF', DYJetsToLL_M10to50_LO, var_WJ_SF, cut_WJ_SF, *bins)
+    h_DY_mlt50_WJ_SF.Scale(DYJetsToLL_M10to50_LO.weight)
+
+    h_DY_mht50_WJ_SF = hist('DY_mht50_WJ_SF', DYJetsToLL_M50_LO_ext, var_WJ_SF, cut_WJ_SF, *bins)
+    h_DY_mht50_WJ_SF.Scale(DYJetsToLL_M50_LO_ext.weight)
+
+    h_DY_WJ_SF = add('DY_WJ_SF', [h_DY_mlt50_WJ_SF, h_DY_mht50_WJ_SF])
+
+    h_TT_WJ_SF = hist('TT_WJ_SF', TT_pow, var_WJ_SF, cut_WJ_SF, *bins)
+    h_TT_WJ_SF.Scale(TT_pow.weight)
+    
+    h_WJ_SF = hist('WJ_SF', WJetsToLNu_LO_ext, var_WJ_SF, cut_WJ_SF, *bins)
+    h_WJ_SF.Scale(WJetsToLNu_LO_ext.weight)
+
+    h_DY_WJ_SF.Scale(-1)
+    h_TT_WJ_SF.Scale(-1)
+    h_ref_WJ_SF = add('ref_WJ_SF', [h_data_WJ_SF,h_DY_WJ_SF,h_TT_WJ_SF])
+
+    ratio_WJ_SF = h_ref_WJ_SF.histogram.Integral()/h_WJ_SF.histogram.Integral()
+else:
+    ratio_WJ_SF = .5
+
+print 'ratio_WJ_SF = ', ratio_WJ_SF
+h_WJ.Scale(ratio_WJ_SF)
+
+##############
+# QCD estimation
+##############
+
+auto_QCD = False
+
+var_QCD = 'mt_total'
+
+cuts_QCD_B = copy.copy(cuts)
+cuts_QCD_C = copy.copy(cuts)
+cuts_QCD_D = copy.copy(cuts)
+
+cuts_QCD_B['sign'] = cut_ss
+cuts_QCD_C['sign'] = cut_os
+cuts_QCD_D['sign'] = cut_ss
+
+cuts_QCD_B['tau_iso'] = 'l2_byTightIsolationMVArun2v1DBoldDMwLT > 0.5'
+cuts_QCD_C['tau_iso'] = '(l2_byTightIsolationMVArun2v1DBoldDMwLT < 0.5 && l2_byIsolationMVArun2v1DBoldDMwLT > 0.5)'
+cuts_QCD_D['tau_iso'] = '(l2_byTightIsolationMVArun2v1DBoldDMwLT < 0.5 && l2_byIsolationMVArun2v1DBoldDMwLT > 0.5)'
+
+cut_QCD_B = str(cuts_QCD_B)
+cut_QCD_B = '({cut})*({weight})'.format(cut=cut_QCD_B,weight=weight)
+cut_QCD_C = str(cuts_QCD_C)
+cut_QCD_C = '({cut})*({weight})'.format(cut=cut_QCD_C,weight=weight)
+cut_QCD_D = str(cuts_QCD_D)
+cut_QCD_D = '({cut})*({weight})'.format(cut=cut_QCD_D,weight=weight)
+
+
+h_data1_QCD_B = hist('data1_B', data1, var_QCD, cut_QCD_B, *bins)
+h_data2_QCD_B = hist('data2_B', data2, var_QCD, cut_QCD_B, *bins)
+h_data3_QCD_B = hist('data3_B', data3, var_QCD, cut_QCD_B, *bins)
+h_data4_QCD_B = hist('data4_B', data4, var_QCD, cut_QCD_B, *bins)
+
+all_data_QCD_B = [h_data1_QCD_B, h_data2_QCD_B, h_data3_QCD_B, h_data4_QCD_B]
+
+h_data_QCD_B = add('data_QCD_B', all_data_QCD_B)
+h_data_QCD_B.Scale(1)
+
+
+h_DY_mlt50_QCD_B   = hist('DY_mlt50_QCD_B', DYJetsToLL_M10to50_LO, var_QCD, cut_QCD_B, *bins)
+h_DY_mlt50_QCD_B.Scale(DYJetsToLL_M10to50_LO.weight)
+    
+h_DY_mht50_QCD_B = hist('DY_mht50_QCD_B', DYJetsToLL_M50_LO_ext, var_QCD, cut_QCD_B, *bins)
+h_DY_mht50_QCD_B.Scale(DYJetsToLL_M50_LO_ext.weight)
+    
+h_DY_QCD_B = add('DY_QCD_B', [h_DY_mlt50_QCD_B, h_DY_mht50_QCD_B])
+
+h_TT_QCD_B = hist('TT_QCD_B', TT_pow, var_QCD, cut_QCD_B, *bins)
+h_TT_QCD_B.Scale(TT_pow.weight)
+
+h_WJ_QCD_B = hist('WJ_QCD_B', WJetsToLNu_LO_ext, var_QCD, cut_QCD_B, *bins)
+h_WJ_QCD_B.Scale(WJetsToLNu_LO_ext.weight*ratio_WJ_SF)
+
+h_DY_QCD_B.Scale(-1)
+h_TT_QCD_B.Scale(-1)
+h_WJ_QCD_B.Scale(-1)
+
+h_ref_QCD_B = add('ref_QCD_B', [h_data_QCD_B,h_DY_QCD_B,h_TT_QCD_B])
+
+if auto_QCD:
+    h_data1_QCD_C = hist('data1_C', data1, var_QCD, cut_QCD_C, *bins)
+    h_data2_QCD_C = hist('data2_C', data2, var_QCD, cut_QCD_C, *bins)
+    h_data3_QCD_C = hist('data3_C', data3, var_QCD, cut_QCD_C, *bins)
+    h_data4_QCD_C = hist('data4_C', data4, var_QCD, cut_QCD_C, *bins)
+    
+    all_data_QCD_C = [h_data1_QCD_C, h_data2_QCD_C, h_data3_QCD_C, h_data4_QCD_C]
+    
+    h_data_QCD_C = add('data_QCD_C', all_data_QCD_C)
+    h_data_QCD_C.Scale(1)
+    
+    h_data1_QCD_D = hist('data1_D', data1, var_QCD, cut_QCD_D, *bins)
+    h_data2_QCD_D = hist('data2_D', data2, var_QCD, cut_QCD_D, *bins)
+    h_data3_QCD_D = hist('data3_D', data3, var_QCD, cut_QCD_D, *bins)
+    h_data4_QCD_D = hist('data4_D', data4, var_QCD, cut_QCD_D, *bins)
+    
+    all_data_QCD_D = [h_data1_QCD_D, h_data2_QCD_D, h_data3_QCD_D, h_data4_QCD_D]
+    
+    h_data_QCD_D = add('data_QCD_D', all_data_QCD_D)
+    h_data_QCD_D.Scale(1)
+    
+    
+    h_DY_mlt50_QCD_C   = hist('DY_mlt50_QCD_C', DYJetsToLL_M10to50_LO, var_QCD, cut_QCD_C, *bins)
+    h_DY_mlt50_QCD_C.Scale(DYJetsToLL_M10to50_LO.weight)
+    
+    h_DY_mht50_QCD_C = hist('DY_mht50_QCD_C', DYJetsToLL_M50_LO_ext, var_QCD, cut_QCD_C, *bins)
+    h_DY_mht50_QCD_C.Scale(DYJetsToLL_M50_LO_ext.weight)
+    
+    h_DY_QCD_C = add('DY_QCD_C', [h_DY_mlt50_QCD_C, h_DY_mht50_QCD_C])
+    
+    h_TT_QCD_C = hist('TT_QCD_C', TT_pow, var_QCD, cut_QCD, *bins)
+    h_TT_QCD_C.Scale(TT_pow.weight)
+    
+    h_WJ_QCD_C = hist('WJ_QCD_C', WJetsToLNu_LO_ext, var_QCD, cut_QCD_C, *bins)
+    h_WJ_QCD_C.Scale(WJetsToLNu_LO_ext.weight*ratio_WJ_SF)
+    
+    h_DY_QCD_C.Scale(-1)
+    h_TT_QCD_C.Scale(-1)
+    h_WJ_QCD_C.Scale(-1)
+    
+    h_ref_QCD_C = add('ref_QCD_C', [h_data_QCD_C,h_DY_QCD_C,h_TT_QCD_C])
+    
+    
+    h_DY_mlt50_QCD_D   = hist('DY_mlt50_QCD_D', DYJetsToLL_M10to50_LO, var_QCD, cut_QCD_D, *bins)
+    h_DY_mlt50_QCD_D.Scale(DYJetsToLL_M10to50_LO.weight)
+    
+    h_DY_mht50_QCD_D = hist('DY_mht50_QCD_D', DYJetsToLL_M50_LO_ext, var_QCD, cut_QCD_D, *bins)
+    h_DY_mht50_QCD_D.Scale(DYJetsToLL_M50_LO_ext.weight)
+    
+    h_DY_QCD_D = add('DY_QCD_D', [h_DY_mlt50_QCD_D, h_DY_mht50_QCD_D])
+    
+    h_TT_QCD_D = hist('TT_QCD_D', TT_pow, var_QCD, cut_QCD_D, *bins)
+    h_TT_QCD_D.Scale(TT_pow.weight)
+    
+    h_WJ_QCD_D = hist('WJ_QCD_D', WJetsToLNu_LO_ext, var_QCD, cut_QCD_D, *bins)
+    h_WJ_QCD_D.Scale(WJetsToLNu_LO_ext.weight*ratio_WJ_SF)
+    
+    h_DY_QCD_D.Scale(-1)
+    h_TT_QCD_D.Scale(-1)
+    h_WJ_QCD_D.Scale(-1)
+    
+    h_ref_QCD_D = add('ref_QCD_D', [h_data_QCD_D,h_DY_QCD_D,h_TT_QCD_D])
+    
+    ratio_QCD = h_ref_QCD_C.histogram.Integral()/h_ref_QCD_D.histogram.Integral()
+else:
+    ratio_QCD = 1.06 # Twiki
+    
+print 'ratio_QCD C/D = ', ratio_QCD
+
+h_ref_QCD_B.Scale(ratio_QCD)
+h_QCD = h_ref_QCD_B
 
 ##############
 # Fast plot
 ##############
 
-h_data.Draw()
+h_data.stack = False
+plotter = Plotter([h_data, h_DY, h_WJ, h_TT, h_QCD], lumi)
+plotter.draw(var, 'a.u.')
+
 
