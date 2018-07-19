b9c6551e4da2252c50864b31f02541ebf291b15a
lucastorterotot <lucas.torterotot@gmail.com>
2018-07-19 11:52:43 +0200

    corrected triggers

--- cuts/mt_triggers.py
+++ cuts/mt_triggers.py
@@ -2,16 +2,28 @@ from htt_plot.tools.cut import Cut, CutFlow
 import pprint
 
 triggers_list = [
-    'trigger_matched_isomu22',
-    'trigger_isotkmu22',
     'trigger_isomu22',
+    'trigger_isotkmu22',
+    'trigger_isomu19tau20'
+    ]
+
+triggers_match_list = [
+    'trigger_matched_isomu22',
     'trigger_matched_isotkmu22',
     'trigger_matched_isomu19tau20',
     'trigger_matched_singlemuon',
     ]
 
-any_trigger_str = '||'.join(triggers_list)
+trigger_str = ' || '.join(triggers_list)
+trigger_match_str = ' || '.join(triggers_match_list)
+
+triggers_with_match_list = []
+for k in range(0,min(len(triggers_list),len(triggers_match_list))):
+    trigger_with_match = '('+' && '.join([triggers_list[k],triggers_match_list[k]])+')'
+    triggers_with_match_list.append(trigger_with_match)
+
+mt_trigger_str = ' || '.join(triggers_with_match_list)
 
 triggers = CutFlow(
-    [('triggers', any_trigger_str)]
+    [('triggers', mt_trigger_str)]
 )
