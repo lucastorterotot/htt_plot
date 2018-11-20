5cbbd4718248e26b5c578df894975ce5bc3e8fc2
Colin Bernet <colin.bernet@cern.ch>
2018-11-20 15:35:44 +0100

    cleanup

--- macros/plot_inclusive_test.py
+++ macros/plot_inclusive_test.py
@@ -61,63 +61,25 @@ all_data = [data1, data2, data3, data4]
 data = add('data', all_data)
 if config.parallel:
     print 'running'
-    # h_data = h_data.compute(scheduler='single-threaded')
     data = data.compute()
 print data.GetEntries()
 print data.Integral()
-#data.Scale(1/nevents_fraction)
-#h_data.Draw()
 
 ##############
 # MC histos
 ##############
-##
 
 ## DY
-
-##DY_mlt50   = hist('DY_mlt50', DYJetsToLL_M10to50_LO, var, cut, *bins)
-##DY_mlt50.Scale(DYJetsToLL_M10to50_LO.weight)
 
 DY_mht50_1 = hist('DY_mht50_1', DYJetsToLL_M50_LO_ext, var, cut, *bins)
 DY_mht50_2 = hist('DY_mht50_2', DYJetsToLL_M50_LO_ext2, var, cut, *bins)
 DY = add('DY', [DY_mht50_1,DY_mht50_2])
 lumi_eq = DYJetsToLL_M50_LO_ext.lumi_eq() + DYJetsToLL_M50_LO_ext2.lumi_eq()
-print 'INTEGRAL BEFORE SCALE', DY.Integral()
 DY = scale(DY, lumi/lumi_eq)
 if config.parallel:
     DY = DY.compute()
-print 'LUMI', lumi, lumi_eq,  DYJetsToLL_M50_LO_ext.lumi_eq(),  DYJetsToLL_M50_LO_ext2.lumi_eq()
-##DY_mht50.Scale(sumMCweights([DYJetsToLL_M50_LO_ext,DYJetsToLL_M50_LO_ext2]))
 print DY.GetEntries()
 print DY.Integral()
-#h_DY.Draw()
-
-## TT
-##
-##TT = hist('TT', TT_pow, var, cut, *bins)
-##TT.Scale(TT_pow.weight)
-##
-##print TT.GetEntries()
-##print TT.Integral()
-###TT_pow.Draw()
-##
-#### WJ
-##
-##WJ1 = hist('WJ1', WJetsToLNu_LO, var, cut, *bins)
-##WJ2 = hist('WJ2', WJetsToLNu_LO_ext, var, cut, *bins)
-##
-##WJ = add('WJ', [WJ1, WJ2])
-##WJ.Scale(sumMCweights([WJetsToLNu_LO,WJetsToLNu_LO_ext]))
-##
-##print WJ.GetEntries()
-##print WJ.Integral()
-##
-##background = add('background',[DY,TT,WJ])
-##if config.parallel:
-##    DY = DY.compute()
-##    TT = TT.compute()
-##    WJ = WJ.compute()
-##    background = background.compute()
 
 data.stack = False
 plotter = Plotter([data, DY], lumi)
