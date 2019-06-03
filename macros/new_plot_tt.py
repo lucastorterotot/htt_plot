c9f005a95ff86866545ea2ec4f6c4f92251d87e9
lucastorterotot <lucas.torterotot@gmail.com>
2019-06-03 17:57:05 +0200

    importing functions rather than module

--- macros/new_plot_tt.py
+++ macros/new_plot_tt.py
@@ -30,67 +30,67 @@ output_dir = 'delayed_plots_tt'
 # Cfgs and components
 #########
 
-import htt_plot.tools.builder as builder
+from htt_plot.tools.builder import build_cfgs, merge_cfgs
 
 # MC
-singleTop_cfgs = builder.build_cfgs(
+singleTop_cfgs = build_cfgs(
     [dataset.name for dataset in datasets.singleTop_datasets], 
     datasets.singleTop_datasets, variables, signal_region_MC_nofakes, bins)
 
-WJ_cfgs = builder.build_cfgs(
+WJ_cfgs = build_cfgs(
     [dataset.name for dataset in datasets.WJ_datasets], 
     datasets.WJ_datasets, variables, signal_region_MC_nofakes, bins)
 
-Diboson_cfgs = builder.build_cfgs(
+Diboson_cfgs = build_cfgs(
     [dataset.name for dataset in datasets.Diboson_datasets], 
     datasets.Diboson_datasets, variables, signal_region_MC_nofakes, bins)
 
-TT_cfgs = builder.build_cfgs(
+TT_cfgs = build_cfgs(
     [dataset.name for dataset in datasets.TT_datasets], 
     datasets.TT_datasets, variables, signal_region_MC_nofakes_TT, bins)
 
-DY_cfgs = builder.build_cfgs(
+DY_cfgs = build_cfgs(
     [dataset.name for dataset in datasets.DY_datasets], 
     datasets.DY_datasets, variables, signal_region_MC_nofakes_DY, bins)
 
-singleTop_comp = builder.merge_cfgs('singleTop',singleTop_cfgs)
-WJ_comp = builder.merge_cfgs('WJ',WJ_cfgs)
-Diboson_comp = builder.merge_cfgs('Diboson',Diboson_cfgs)
-TT_comp = builder.merge_cfgs('TTBar',TT_cfgs)
-DY_comp = builder.merge_cfgs('DY',DY_cfgs)
+singleTop_comp = merge_cfgs('singleTop',singleTop_cfgs)
+WJ_comp = merge_cfgs('WJ',WJ_cfgs)
+Diboson_comp = merge_cfgs('Diboson',Diboson_cfgs)
+TT_comp = merge_cfgs('TTBar',TT_cfgs)
+DY_comp = merge_cfgs('DY',DY_cfgs)
 
 MC_components = [singleTop_comp, WJ_comp, Diboson_comp, TT_comp, DY_comp]
 
 # data
-data_cfgs = builder.build_cfgs(
+data_cfgs = build_cfgs(
     [dataset.name for dataset in datasets.data_datasets], 
     datasets.data_datasets, variables, signal_region, bins)
 for cfg in data_cfgs:
     cfg.stack = False
-data_component = builder.merge_cfgs('data', data_cfgs)
+data_component = merge_cfgs('data', data_cfgs)
 
 # Embedded
-Embedded_cfgs = builder.build_cfgs(
+Embedded_cfgs = build_cfgs(
     [dataset.name for dataset in datasets.Embedded_datasets], 
     datasets.Embedded_datasets, variables, signal_region_Embedded, bins)
-Embedded_component = builder.merge_cfgs('Embedded', Embedded_cfgs)
+Embedded_component = merge_cfgs('Embedded', Embedded_cfgs)
 
 # fakes
 datasets_MC_fakes = datasets.WJ_datasets + datasets.Diboson_datasets + datasets.singleTop_datasets + datasets.DY_datasets + datasets.TT_datasets
-fake_cfgs_MC_1 = builder.build_cfgs(['fakesMC1'], datasets_MC_fakes, variables, l1_FakeFactorApplication_Region_genuinetauMC, bins)
-fake_cfgs_MC_2 = builder.build_cfgs(['fakesMC2'], datasets_MC_fakes, variables, l2_FakeFactorApplication_Region_genuinetauMC, bins)
+fake_cfgs_MC_1 = build_cfgs(['fakesMC1'], datasets_MC_fakes, variables, l1_FakeFactorApplication_Region_genuinetauMC, bins)
+fake_cfgs_MC_2 = build_cfgs(['fakesMC2'], datasets_MC_fakes, variables, l2_FakeFactorApplication_Region_genuinetauMC, bins)
 
-fake_cfgs_1 = builder.build_cfgs(
+fake_cfgs_1 = build_cfgs(
     ['fakes'+dataset.name[-1]+'1' for dataset in datasets.data_datasets], 
     datasets.data_datasets, variables, l1_FakeFactorApplication_Region, bins)
-fake_cfgs_2 = builder.build_cfgs(
+fake_cfgs_2 = build_cfgs(
     ['fakes'+dataset.name[-1]+'2' for dataset in datasets.data_datasets], 
     datasets.data_datasets, variables, l2_FakeFactorApplication_Region, bins)
 
-fake_cfgs_Embedded_1 = builder.build_cfgs(
+fake_cfgs_Embedded_1 = build_cfgs(
     ['fakesEmbedded'+dataset.name[-1]+'1' for dataset in datasets.Embedded_datasets], 
     datasets.Embedded_datasets, variables, l1_FakeFactorApplication_Region_genuinetauMC_Embedded, bins)
-fake_cfgs_Embedded_2 = builder.build_cfgs(
+fake_cfgs_Embedded_2 = build_cfgs(
     ['fakesEmbedded'+dataset.name[-1]+'2' for dataset in datasets.Embedded_datasets], 
     datasets.Embedded_datasets, variables, l2_FakeFactorApplication_Region_genuinetauMC_Embedded, bins)
 
@@ -98,7 +98,7 @@ for cfg in fake_cfgs_MC_1+fake_cfgs_MC_2+fake_cfgs_Embedded_1+fake_cfgs_Embedded
     cfg.scale = -1.
 
 fakes_cfgs = fake_cfgs_1+fake_cfgs_2+fake_cfgs_Embedded_1+fake_cfgs_Embedded_2+fake_cfgs_MC_1+fake_cfgs_MC_2
-fakes_component = builder.merge_cfgs('fakes',fakes_cfgs)
+fakes_component = merge_cfgs('fakes',fakes_cfgs)
 
 all_comp =  MC_components+[data_component]+[Embedded_component]+[fakes_component]
 
