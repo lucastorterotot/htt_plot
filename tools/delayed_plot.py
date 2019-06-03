2a2ddce9e2987e50e5adceea128b64f4d6e2b13b
lucastorterotot <lucas.torterotot@gmail.com>
2019-06-03 10:53:08 +0200

    bakup

---
+++ tools/delayed_plot.py
@@ -0,0 +1,18 @@
+from htt_plot.tools.cut import Cut
+from htt_plot.tools.delayed_component import Component_cfg
+import copy
+
+def build_component(name, datasets, var, cut, *bins):
+    if isinstance(cut,Cut):
+        cut = cut.cutstr
+    datasets = copy.deepcopy(datasets)
+    comp = Component_cfg(name, datasets, var, cut, *bins)
+    print name
+    print datasets
+    return comp
+
+def build_components(names, datasets, var, cut, *bins):
+    components = []
+    for name, dataset in zip(names, datasets):
+        components.append(build_component(name,dataset,var,cut,*bins))
+    return components
