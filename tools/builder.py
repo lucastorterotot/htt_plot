a26e19b4829a1b78ec205b8dbb8c9f4ed0368371
lucastorterotot <lucas.torterotot@gmail.com>
2019-06-11 11:02:31 +0200

    One dataset per cfg

--- tools/builder.py
+++ tools/builder.py
@@ -4,10 +4,10 @@ from dask import delayed
 import copy
 from ROOT import TH1F
 
-def build_cfg(name, datasets, var, cut, *bins):
+def build_cfg(name, dataset, var, cut, *bins):
     if isinstance(cut,Cut):
         cut = cut.cutstr
-    cfg = Component_cfg(name, datasets, var, cut, *bins)
+    cfg = Component_cfg(name, dataset, var, cut, *bins)
     return cfg
 
 def build_cfgs(names, datasets, var, cut, *bins):
@@ -36,30 +36,15 @@ def merge_components(name, comps):
 
 merge_components = delayed(merge_components)
 
-def project(comp, verbose=False):
-    for dataset in comp.cfg.datasets:
-        for var in comp.cfg.variables:
-            histo = TH1F(comp.cfg.name+dataset.name+var, comp.cfg.name+dataset.name, *comp.cfg.bins[var])
-            if not hasattr(dataset.tree,'Project'):
-                import pdb;pdb.set_trace()
-            dataset.tree.Project(comp.cfg.name+dataset.name+var, var, comp.cfg.cut)
-            if histo.Integral() == 0 and 'fake' in comp.name:
-                print '\n', 'component :',comp.cfg.name, 'dataset :',dataset.name
-                print dataset
-                print dataset.tree
-            if verbose:
-                print '\n', 'component :',comp.cfg.name, 'dataset :',dataset.name
-                if not dataset.is_data:
-                    print 'lumi prescale:', dataset.nevts/dataset.xsection
-                    print 'lumiweight:', dataset.weight
-                print 'integral prescale:', histo.Integral()
-            histo.Scale(dataset.weight)
-            histo.Scale(comp.cfg.scale)
-            if verbose:
-                print 'integral postscale:', histo.Integral()
-            comp.histogram[var].Add(histo)
+def project(comp):
+    dataset = comp.cfg.dataset
+    for var in comp.cfg.variables:
+        histo = TH1F(comp.name+dataset.name+var, comp.name+dataset.name, *comp.cfg.bins[var])
+        dataset.tree.Project(comp.name+dataset.name+var, var, comp.cfg.cut)
+        histo.Scale(dataset.weight * comp.cfg.scale)
+        comp.histogram[var].Add(histo)
 
 def merge(comp, others):
     for var in comp.cfg.variables:
         for other in others:
-            comp.histogram[var].Add(other.histogram[var]) 
+            comp.histogram[var].Add(other.histogram[var])
--- tools/component.py
+++ tools/component.py
@@ -6,23 +6,17 @@ class Component(object):
           self.cfg = component_cfg
           self.name = self.cfg.name
           for var in self.cfg.variables:
-               self.histogram[var] = TH1F(self.name+var, self.name+var, *self.cfg.bins[var])
+               self.histogram[var] = TH1F(self.name+'_'+var, self.name+'_'+var, *self.cfg.bins[var])
                
 class Component_cfg(object):
      
-     def __init__(self, name, datasets, variables, cut, bins):
+     def __init__(self, name, dataset, variables, cut, bins):
           self.name = name
           self.variables = variables
           self.cut = cut
           self.bins = bins
           self.stack = True
-          if not isinstance(datasets, list):
-               self.datasets = [datasets]
-          else:
-               self.datasets = datasets
+          self.dataset = dataset
           self.nevts = 0
-          if not all([ds.xsection==self.datasets[0].xsection for ds in self.datasets]):
-               print '!!!xsection not same in all datasets!!!'
-               import pdb;pdb.set_trace()
-          self.xsection = self.datasets[0].xsection
+          self.xsection = self.dataset.xsection
           self.scale = 1
