d4c72350e9a2572da5d447723105c08410559f0d
lucastorterotot <lucas.torterotot@gmail.com>
2019-06-11 13:12:36 +0200

    Change in datacards.py to avoid modification on preexisting TH1F

--- tools/datacards.py
+++ tools/datacards.py
@@ -1,12 +1,11 @@
-from ROOT import TFile
+from ROOT import TFile, TH1F
 
 def make_datacards(output_dir, variable, **kwargs):
     ''' kwargs are components which names are the future TH1 names '''
     rootfile = TFile(output_dir+'/datacards_'+variable+'.root', 'recreate')
     for key in kwargs.keys():
         component = kwargs[key]
-        hist = component.histogram[variable]
-        hist.SetName(key)
-        hist.SetTitle('Datacard '+key+' for var '+variable)
+        hist = TH1F(key, 'Datacard '+key+' for var '+variable, *component.cfg.bins[variable])
+        hist.Add(component.histogram[variable])
         hist.Write()
     rootfile.Close()
