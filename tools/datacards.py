ace0790cf87ee980874258f8c60da7a4dda66bc6
lucastorterotot <lucas.torterotot@gmail.com>
2019-06-13 15:03:28 +0200

    using Clone method for hists in datacards maker

--- tools/datacards.py
+++ tools/datacards.py
@@ -3,7 +3,8 @@ from ROOT import TFile, TH1F
 def make_datacards(output_dir, variable, components_dict):
     rootfile = TFile(output_dir+'/datacards_'+variable+'.root', 'recreate')
     for key, component in components_dict.items():
-        hist = TH1F(key, 'Datacard '+key+' for var '+variable, *component.cfg.bins)
-        hist.Add(component.histogram)
+        hist = component.histogram.Clone(key)
+        hist.SetTitle(key)
         hist.Write()
     rootfile.Close()
+    print('Datacards for var '+variable+' made.')
