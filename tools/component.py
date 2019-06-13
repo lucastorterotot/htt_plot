69d865db4d4eeb6db3befee8e6b59650a942449b
lucastorterotot <lucas.torterotot@gmail.com>
2019-06-13 16:23:55 +0200

    Added some documentation

--- tools/component.py
+++ tools/component.py
@@ -2,6 +2,9 @@ from ROOT import TH1F
 import copy
 
 class Component(object):
+     ''' Python wrapper for ROOT histograms. Based on a Component_cfg object,
+     this gives access to a histogram. Shortcuts for the name and the variable
+     are also available, yet all information can be found in the cfg attribute.'''
      
      def __init__(self, component_cfg, init_TH1F=True):
           self.cfg = component_cfg
@@ -17,6 +20,16 @@ class Component(object):
           return new
           
 class Component_cfg(dict):
+     ''' A configuration class for components. Gives a dict
+     containing all key words passed at init. Some defaults are
+     provided, yet some key words are necessary, such as
+     - name : component name, used for histograms names
+              so this is important to differenciate
+              differents components
+     - variable : the variable that will be used to plot
+     - dataset : the python class wrapper for the root
+              source file containing the root tree
+     '''
 
      defaults = {
           'cut' : '1',
--- tools/datacards.py
+++ tools/datacards.py
@@ -1,6 +1,12 @@
 from ROOT import TFile, TH1F
 
 def make_datacards(output_dir, variable, components_dict):
+    '''This funciton aims at producing a root file containing all
+    histograms needed for datacards. To do so, provide a dict of components
+    (class Component) that have the ROOT histograms to be used.
+    Gives also the output directory and the variable name, so that you can
+    make datacards for different variables.'''
+    
     rootfile = TFile(output_dir+'/datacards_'+variable+'.root', 'recreate')
     for key, component in components_dict.items():
         hist = component.histogram.Clone(key)
