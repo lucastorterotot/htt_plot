790857a3ab33d4aa930b7e780d7f35c3e77acd59
lucastorterotot <lucas.torterotot@gmail.com>
2019-06-19 15:34:19 +0200

    Removed cut dir, unused

--- cuts/htt_triggers_ot.py
+++
@@ -1,7 +0,0 @@
-from htt_plot.tools.cut import Cuts
-
-cuts_tt = Cuts(
-    doubletau_lowpt = 'trg_doubletau_lowpt',
-    doubletau_mediso = 'trg_doubletau_mediso',
-    doubletau = 'trg_doubletau',
-)
--- cuts/htt_vetoes.py
+++
@@ -1,7 +0,0 @@
-from htt_plot.tools.cut import Cuts
-
-cuts_vetoes = Cuts(
-    dileptonveto = '!veto_dilepton', 
-    thirdleptonveto = '!veto_extra_elec', 
-    otherleptonveto = '!veto_extra_muon', 
-)
