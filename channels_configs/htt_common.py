7d49868d937ff2e4764c91db4398ba9f931e157e
lucastorterotot <lucas.torterotot@gmail.com>
2019-06-19 14:27:05 +0200

    config files updated for tt

---
+++ channels_configs/htt_common.py
@@ -0,0 +1,72 @@
+''' General config file for plotting and datacards.
+HTT common config
+'''
+from htt_plot.tools.cut import Cut, Cuts
+
+# binning
+from array import array
+bins = {
+    'l1_eta' : (50, -2.5, 2.5),
+    'l1_pt'  : (32, 0., 160.),
+    'l2_eta' : (50, -2.5, 2.5),
+    'l2_pt'  : (32, 0., 160.),
+    'met'  : (32, 0., 160.),
+    'mt_tot' : (30, 0., 300.),
+    'm_vis'   : (32, 0., 200.)
+}
+
+# variables
+variables = bins.keys()
+datacards_variables = ['mt_tot']
+variables = datacards_variables+[variables[0]] # just for testing
+
+# common cuts
+cuts_flags = Cuts(
+    goodVertices = 'Flag_goodVertices',
+    TightHalo = 'Flag_globalTightHalo2016Filter',
+    # SuperTightHalo = 'Flag_globalSuperTightHalo2016Filter',
+    Noise = 'Flag_HBHENoiseFilter',
+    NoiseIso = 'Flag_HBHENoiseIsoFilter',
+    EcalDeadCell = 'Flag_EcalDeadCellTriggerPrimitiveFilter',
+    BadPFMuon = 'Flag_BadPFMuonFilter',
+    BadChargedCandidate = 'Flag_BadChargedCandidateFilter',
+    # eeBadSc = 'Flag_eeBadScFilter',
+    ecalBadCalib = 'Flag_ecalBadCalibFilter'
+)
+
+cuts_vetoes = Cuts(
+    dileptonveto = '!veto_dilepton', 
+    thirdleptonveto = '!veto_extra_elec', 
+    otherleptonveto = '!veto_extra_muon', 
+)
+
+
+
+cut_l1_fakejet = Cut('l1_gen_match==6')
+cut_l2_fakejet = Cut('l2_gen_match==6')
+
+cut_os = Cut('l1_q != l2_q')
+cut_ss = ~cut_os
+
+cut_dy_promptfakeleptons = Cut(
+    'l1_gen_match==1 || l1_gen_match==2 || l2_gen_match==1 || l2_gen_match==2')
+
+cut_TT_nogenuine = Cut('!(l1_gen_match==5 && l2_gen_match==5)')
+
+
+
+cut_mt_tot = Cut('mt_tot < 40')
+
+cut_btag_1 = Cut('bjet1_csv > 0')
+
+cut_btag_2 = Cut('bjet2_csv > 0')
+
+# weights
+weights = Cuts(
+    weight = 'weight',
+    MC = 'l1_weight_mutotaufake_loose * l1_weight_etotaufake_vloose * l1_weight_tauid_vtight * l2_weight_mutotaufake_loose * l2_weight_etotaufake_vloose * l2_weight_tauid_vtight',
+    DY = 'weight_dy * weight_generator',# * '+dy_stitching_weight
+    embed = 'weight_embed_DoubleMuonHLT_eff * weight_embed_muonID_eff_l1 * weight_embed_muonID_eff_l2 * weight_embed_DoubleTauHLT_eff_l1 * weight_embed_DoubleTauHLT_eff_l2 * weight_embed_track_l1 * weight_embed_track_l2',
+    l1_fake = 'l1_fakeweight*0.5',
+    l2_fake = 'l2_fakeweight*0.5'
+    )
--- channels_configs/tt.py
+++ channels_configs/tt.py
@@ -1,47 +1,98 @@
 ''' General config file for plotting and datacards.
 tau-tau channel
 '''
-
-from htt_plot.tools.cut import Cuts
+from htt_plot.tools.cut import Cut, Cuts
 
 channel = 'tt'
 
 # binning
-from htt_plot.binning import bins
+from htt_plot.channels_configs.htt_common import bins
 
 # variables
-variables = bins.keys()
-datacards_variables = ['mt_tot']
-variables = datacards_variables+[variables[0]] # just for testing
+from htt_plot.channels_configs.htt_common import variables, datacards_variables
 
 # cuts
-from htt_plot.cuts.htt_cuts import cuts_tt as cut_signal
-from htt_plot.cuts.htt_cuts import cuts_against_leptons_tt as cuts_against_leptons
+from htt_plot.channels_configs.htt_common import cut_mt_tot, cuts_flags, cuts_vetoes, cut_l1_fakejet, cut_l2_fakejet, cut_os, cut_ss, cut_dy_promptfakeleptons, cut_TT_nogenuine, cut_btag_1, cut_btag_2
 
-from htt_plot.cuts.htt_triggers_ot import cuts_tt as triggers
-triggers = Cuts(triggers = triggers.any())
+cuts_l1 = Cuts(
+    l1_pt = 'l1_pt > 40',
+    l1_eta = 'abs(l1_eta) < 2.1',
+    l1_iso = 'l1_byVVLooseIsolationMVArun2017v2DBoldDMwLT2017 > 0.5',
+    l1_charge = 'abs(l1_q) == 1.',
+    l1_vertex = 'abs(l1_dz) < 0.2',
+    l1_decaymode = 'l1_decayModeFinding > 0.5',
+)
 
-from htt_plot.cuts.htt_isolations import cuts_iso_tt as cuts_iso
+cuts_l2 = Cuts(
+    l2_pt = 'l2_pt > 40',
+    l2_eta = 'abs(l2_eta) < 2.1',
+    l2_iso = 'l2_byVVLooseIsolationMVArun2017v2DBoldDMwLT2017 > 0.5',
+    l2_charge = 'abs(l2_q) == 1.',
+    l2_vertex = 'abs(l2_dz) < 0.2',
+    l2_decaymode = 'l2_decayModeFinding > 0.5',
+)
 
-from htt_plot.cuts.htt_flags import cuts_flags
-from htt_plot.cuts.htt_vetoes import cuts_vetoes
-from htt_plot.cuts.htt_generic import cut_l1_fakejet, cut_l2_fakejet, cut_os, cut_ss, cut_dy_promptfakeleptons, cut_TT_nogenuine
+cuts_against_leptons = Cuts(
+    l1_against_e = 'l1_againstElectronVLooseMVA6 > 0.5',
+    l1_against_mu = 'l1_againstMuonLoose3 > 0.5',
+    l2_against_e = 'l2_againstElectronVLooseMVA6 > 0.5',
+    l2_against_mu = 'l2_againstMuonLoose3 > 0.5',
+)
 
-basic_cuts = cuts_flags + cuts_vetoes + triggers + cut_os + cuts_against_leptons
+cut_signal = cuts_l1.all() & cuts_l2.all() & cuts_against_leptons.all() & cut_mt_tot
 
-from htt_plot.cuts.htt_datacards_cuts import cuts_tt as cuts_datacards
+## triggers
+cuts_triggers = Cuts(
+    singletau = 'trg_singletau',
+    doubletau_35_mediso = 'trg_doubletau_35_mediso',
+    doubletau_35_tightiso_tightid = 'trg_doubletau_35_tightiso_tightid',
+    doubletau_40_mediso_tightid = 'trg_doubletau_40_mediso_tightid',
+    doubletau_40_tightiso = 'trg_doubletau_40_tightiso',
+    )
+# tmp hack for old variable names
+cuts_triggers = Cuts(
+    doubletau_lowpt = 'trg_doubletau_lowpt',
+    doubletau_mediso = 'trg_doubletau_mediso',
+    doubletau = 'trg_doubletau',
+)
+
+cut_triggers = cuts_triggers.any()
+
+basic_cuts = cuts_flags.all() & cuts_vetoes.all() & cut_triggers & cut_os & cuts_against_leptons.all()
+
+## iso
+cuts_iso = Cuts(
+    l1_VTight = 'l1_byVTightIsolationMVArun2017v2DBoldDMwLT2017 > 0.5',
+    l1_Tight = 'l1_byTightIsolationMVArun2017v2DBoldDMwLT2017 > 0.5',
+    l1_VLoose = 'l1_byVLooseIsolationMVArun2017v2DBoldDMwLT2017 > 0.5',
+    l1_VVLoose = 'l1_byVVLooseIsolationMVArun2017v2DBoldDMwLT2017 > 0.5',
+    l2_VTight = 'l2_byVTightIsolationMVArun2017v2DBoldDMwLT2017 > 0.5',
+    l2_Tight = 'l2_byTightIsolationMVArun2017v2DBoldDMwLT2017 > 0.5',
+    l2_VLoose = 'l2_byVLooseIsolationMVArun2017v2DBoldDMwLT2017 > 0.5',
+    l2_VVLoose = 'l2_byVVLooseIsolationMVArun2017v2DBoldDMwLT2017 > 0.5',
+)
+
+## datacards
+cuts_datacards = Cuts(
+    ZTT = 'l1_gen_match == 5 && l2_gen_match == 5',
+    ZL = 'l1_gen_match < 6 && l2_gen_match < 6 && !(l1_gen_match == 5 && l2_gen_match == 5)',
+    ZJ = 'l1_gen_match == 6 || l2_gen_match == 6',
+    TTT = 'l1_gen_match == 5 && l2_gen_match == 5',
+    TTJ = '!(l1_gen_match == 5 && l2_gen_match == 5)',
+    VVT = '(l1_gen_match == 5 && l2_gen_match == 5)',
+    VVJ = '!(l1_gen_match == 5 && l2_gen_match == 5)',
+    W = '1',
+    jetFakes = '1',
+)
+cuts_datacards['ZLL'] = cuts_datacards['ZL'] | cuts_datacards['ZJ']
+cuts_datacards['TT'] = cuts_datacards['TTT'] | cuts_datacards['TTJ']
+cuts_datacards['VV'] = cuts_datacards['VVT'] | cuts_datacards['VVJ']
 
 # weights
-from htt_plot.cuts.htt_weights import weights
+from htt_plot.channels_configs.htt_common import weights
 
 # datasets
 import htt_plot.datasets.gael_all as datasets
 
 ### tmp hack
-cut_signal = Cuts()
-###
-
-for leg in ['l1', 'l2']:
-    cut_signal += cuts_iso[leg+'_Tight']
-
-cut_signal += basic_cuts
+cut_signal = cuts_iso['l1_Tight'] & cuts_iso['l2_Tight'] & basic_cuts
