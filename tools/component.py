4ce263a7acbe1a334e59da5cace908da74a78a07
lucastorterotot <lucas.torterotot@gmail.com>
2019-06-19 15:22:01 +0200

    Comments taken into account for tools component

--- tools/component.py
+++ tools/component.py
@@ -1,43 +1,34 @@
 from ROOT import TH1F
 import copy
+from itertools import count
 
 class Component(object):
      ''' Python wrapper for ROOT histograms. Based on a Component_cfg object,
      this gives access to a histogram. Shortcuts for the name and the variable
      are also available, yet all information can be found in the cfg attribute.'''
+     _ids = count(0)
      
-     def __init__(self, component_cfg, init_TH1F=True):
-          # TODO doc
+     def __init__(self, component_cfg):
+          '''Init with a Component_cfg object, which must have at least a name and
+          a variable'''
+          self.id = next(self._ids)
           self.cfg = component_cfg
           self.name = self.cfg['name']
           self.var = self.cfg['variable']
-          # TODO : ensure unique histo name : counter of compnent objects
-          # https://stackoverflow.com/questions/8628123/counting-instances-of-a-class
-          self.histogram = TH1F(self.name+'_'+self.var, self.name+'_'+self.var, *self.cfg['bins'])
+          TH1F_str = '_'.join([self.name, self.var, str(self.id)])
+          self.histogram = TH1F(TH1F_str, TH1F_str, *self.cfg['bins'])
           
      def Clone(self, name):
-          # TODO doc
+          '''Get a new component wich only differs from the previous by its name and id'''
           new = copy.copy(self)
           new.name = name
-          # avoid useless joins
-          new.histogram = self.histogram.Clone(new.name+'_'+new.var)
-          new.histogram.SetTitle(new.name+'_'+new.var)
+          new.id = next(self._ids)
+          TH1F_str = '_'.join([new.name, new.var, str(new.id)])
+          new.histogram = self.histogram.Clone(TH1F_str)
+          new.histogram.SetTitle(TH1F_str)
           return new
           
 class Component_cfg(dict):
-     # TODO : pass it in the init fct
-     # xsection to be given or contained in daatset
-     ''' A configuration class for components. Gives a dict
-     containing all key words passed at init. Some defaults are
-     provided, yet some key words are necessary, such as
-     - name : component name, used for histograms names
-              so this is important to differenciate
-              differents components
-     - variable : the variable that will be used to plot
-     - dataset : the python class wrapper for the root
-              source file containing the root tree
-     '''
-
      defaults = {
           'cut' : '1',
           'bins' : (30, 0., 300.),
@@ -47,9 +38,20 @@ class Component_cfg(dict):
      }
      
      def __init__(self, **kwargs):
-          # TODO : use update method of dict and then __init__ only once
-          super(Component_cfg, self).__init__(self.__class__.defaults)
-          super(Component_cfg, self).__init__(kwargs)
+          ''' A configuration class for components. Gives a dict
+          containing all key words passed at init. Some defaults are
+          provided, yet some keywords are necessary, such as
+          - name : component name, used for histograms names
+              so this is important to differenciate
+              differents components
+          - variable : the variable that will be used to plot
+          - dataset : the python class wrapper for the root
+              source file containing the root tree
+          - xsection : if not provided, dataset.xsection will be used
+          '''
+          init_dict = copy.copy(self.defaults)
+          init_dict.update(kwargs)
+          super(Component_cfg, self).__init__(init_dict)
           if not 'xsection' in self:
                # as user must provide a dataset
                self['xsection'] = self['dataset'].xsection
