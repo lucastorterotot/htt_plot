5ef0965b0eebb87a78505491c7b0156acaa0c874
lucastorterotot <lucas.torterotot@gmail.com>
2019-06-24 11:05:55 +0200

    test_cuts.py update according to Colin's comments

--- tests/test_cuts.py
+++ tests/test_cuts.py
@@ -2,106 +2,85 @@
 
 from htt_plot.tools.cut import Cut, Cuts
 
-cuts_triggers = Cuts(
-    doubletau_lowpt = 'trg_doubletau_lowpt',
-    doubletau_mediso = 'trg_doubletau_mediso',
-    doubletau = 'trg_doubletau',
-)
-
-any_triggers = cuts_triggers.any()
-all_triggers = cuts_triggers.all()
-
-any_triggers_test = Cut(
-    '((trg_doubletau) || (trg_doubletau_mediso) || (trg_doubletau_lowpt))'
-    )
-all_triggers_test = Cut(
-    '((trg_doubletau) && (trg_doubletau_mediso) && (trg_doubletau_lowpt))'
-    )
-
-cuts_datacards = Cuts(
-    VVT = '(l1_gen_match == 5 && l2_gen_match == 5)',
-    VVJ = '!(l1_gen_match == 5 && l2_gen_match == 5)',
-)
-cuts_datacards['VV'] = cuts_datacards['VVT'] | cuts_datacards['VVJ']
-cuts_datacards_VV = Cut(
-    '((l1_gen_match == 5 && l2_gen_match == 5)) || (!(l1_gen_match == 5 && l2_gen_match == 5))'
-    )
-
-cut_os = Cut('l1_q != l2_q')
-cut_ss = ~cut_os
-not_cut_os = Cut('!(l1_q != l2_q)')
-cut_os_and_cut_ss = Cut('(l1_q != l2_q) && (!(l1_q != l2_q))')
-cut_os_or_cut_ss = Cut('(l1_q != l2_q) || (!(l1_q != l2_q))')
-
-weight = Cut('weight')
-
-cut_os_times_weight = Cut('(l1_q != l2_q) * (weight)')
-cut_os_itimes_weight = Cut('l1_q != l2_q')
-cut_os_itimes_weight *= weight
-
 import unittest
 
 class TestCuts(unittest.TestCase):
 
     def test_str(self):
         self.assertEqual(
-            str(weight),
-            'weight',
-            "Cut str fails")
+            str(Cut('weight')),
+            'weight')
 
     def test_and(self):
         self.assertEqual(
-            str(cut_os & cut_ss),
-            str(cut_os_and_cut_ss),
-            "Cut and fails")
+            str(Cut('l1_q != l2_q') & Cut('!(l1_q != l2_q)')),
+            str(Cut('(l1_q != l2_q) && (!(l1_q != l2_q))')))
 
     def test_and_not(self):
         self.assertEqual(
-            str(cut_os & ~cut_os),
-            str(cut_os_and_cut_ss),
-            "Cut and not fails")
+            str(Cut('l1_q != l2_q') & ~Cut('l1_q != l2_q')),
+            str(Cut('(l1_q != l2_q) && (!(l1_q != l2_q))')))
 
     def test_or(self):
         self.assertEqual(
-            str(cut_os | cut_ss),
-            str(cut_os_or_cut_ss),
-            "Cut or fails")
+            str(Cut('l1_q != l2_q') | Cut('!(l1_q != l2_q)')),
+            str(Cut('(l1_q != l2_q) || (!(l1_q != l2_q))')))
 
     def test_not(self):
         self.assertEqual(
-            str(cut_ss),
-            str(not_cut_os),
-            "Cut not fails")
+            str(~Cut('l1_q != l2_q')),
+            str(Cut('!(l1_q != l2_q)')))
 
     def test_mul(self):
         self.assertEqual(
-            str(cut_os*weight),
-            str(cut_os_times_weight),
-            "Cut mul fails")
+            str(Cut('l1_q != l2_q') * Cut('weight')),
+            str(Cut('(l1_q != l2_q) * (weight)')))
 
     def test_imul(self):
+        cut_os_itimes_weight = Cut('l1_q != l2_q')
+        cut_os_itimes_weight *= Cut('weight')
         self.assertEqual(
-            str(cut_os_times_weight),
-            str(cut_os_times_weight),
-            "Cut imul fails")
+            str(cut_os_itimes_weight),
+            str(Cut('(l1_q != l2_q) * (weight)')))
 
     def test_any(self):
+        cuts_triggers = Cuts(
+            doubletau_lowpt = 'trg_doubletau_lowpt',
+            doubletau_mediso = 'trg_doubletau_mediso',
+            doubletau = 'trg_doubletau',
+        )
+        any_triggers_test = Cut(
+            '((trg_doubletau_lowpt) || (trg_doubletau) || (trg_doubletau_mediso))'
+        )
         self.assertEqual(
-            str(any_triggers),
-            str(any_triggers_test),
-            "Cuts any fails")
+            str(cuts_triggers.any()),
+            str(any_triggers_test))
 
     def test_all(self):
+        cuts_triggers = Cuts(
+            doubletau_lowpt = 'trg_doubletau_lowpt',
+            doubletau_mediso = 'trg_doubletau_mediso',
+            doubletau = 'trg_doubletau',
+        )
+        all_triggers_test = Cut(
+            '((trg_doubletau) && (trg_doubletau_mediso) && (trg_doubletau_lowpt))'
+        )
         self.assertEqual(
-            str(all_triggers),
-            str(all_triggers_test),
-            "Cuts all fails")
+            str(cuts_triggers.all()),
+            str(all_triggers_test))
 
     def test_setitem(self):
+        cuts_datacards = Cuts(
+            VVT = '(l1_gen_match == 5 && l2_gen_match == 5)',
+            VVJ = '!(l1_gen_match == 5 && l2_gen_match == 5)',
+        )
+        cuts_datacards['VV'] = cuts_datacards['VVT'] | cuts_datacards['VVJ']
+        cuts_datacards_VV = Cut(
+            '((l1_gen_match == 5 && l2_gen_match == 5)) || (!(l1_gen_match == 5 && l2_gen_match == 5))'
+        )
         self.assertEqual(
             str(cuts_datacards['VV']),
-            str(cuts_datacards_VV),
-            "Cuts setitem fails")
+            str(cuts_datacards_VV))
 
 if __name__ == '__main__':   
     unittest.main()
