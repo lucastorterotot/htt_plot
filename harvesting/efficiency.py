65225c6a5b1f11df7793d1ccbadaffddeb05d092
Colin <colin.bernet@cern.ch>
2019-07-03 11:17:32 +0200

    documentation

--- harvesting/efficiency.py
+++ harvesting/efficiency.py
@@ -5,6 +5,9 @@ pwd = getpass()
 dsdb = DatasetDB('reader', pwd, db='datasets')
 
 def get_nchunks(info):
+    '''returns number of chunks for a dataset info.
+    Sums up all chunks in subdirectories 0000, 0001, etc
+    '''
     tgzs = info.get('tgzs', None)
     if tgzs is None: 
         return 0
@@ -13,7 +16,10 @@ def get_nchunks(info):
         nchunks += len(subdchunks) 
     return nchunks
 
-def efficiency(name): 
+def efficiency(name):
+    '''returns efficiency for dataset with this name.
+    name should be like 190503%HiggsSUSYGG1400%tt_mssm_signals_CMS_scale_j_RelativeSample_13TeV_up
+    '''
     infos = dsdb.find('se', {'name':name})
     if not infos: 
         raise ValueError(name + ' not found in the database')
