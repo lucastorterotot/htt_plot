32ef665870596a145c44ee5c7a8d3e61d608bae6
lucastorterotot <lucas.torterotot@gmail.com>
2020-05-12 13:45:37 +0200

    Update benchmark_script.sh

--- benchmark_script.sh
+++ benchmark_script.sh
@@ -3,9 +3,10 @@
 for Nw in {1..20}
 do
     mkdir -p with_dask_${Nw}_mt
-    /usr/bin/time -v python $(which datacard_and_plot_maker) -s -c mt -o with_dask_$Nw -w $Nw > ./with_dask_${Nw}_mt/time_log.out
+    { /usr/bin/time -v python $(which datacard_and_plot_maker) -s -c mt -o with_dask_$Nw -w $Nw ; } 2> ./with_dask_${Nw}_mt/time_log.out
     tail -n 23 ./with_dask_${Nw}_mt/time_log.out > ./with_dask_${Nw}_mt/time_log_small.out
     mv ./with_dask_${Nw}_mt/time_log_small.out ./with_dask_${Nw}_mt/time_log.out
+    cat ./with_dask_${Nw}_mt/time_log.out
 done
 
     
